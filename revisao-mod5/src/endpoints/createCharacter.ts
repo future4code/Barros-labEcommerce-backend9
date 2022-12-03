1dc6c928b0f611413bc64e89ff907c88ecbd3cf4
DiegoRiosS-dev <diegoriossantos@hotmail.com>
2022-12-03 11:40:50 -0300

    FEAT: Revisao 5 e 6

--- revisao-mod5/src/endpoints/createCharacter.ts
+++ revisao-mod5/src/endpoints/createCharacter.ts
@@ -1,15 +1,22 @@
 import {Request, Response} from "express";
-import { characters } from "../dataBase/data";
+import { connection } from "../dataBase/connection";
 
-export  default function(req:Request, res:Response): void {
-  const {name, gender, description} = req.body;
+export  default async function createCharacter(req:Request, res:Response):Promise<void> {
+  try{
+    const {name, gender, description} = req.body;
 
-  characters.push({
-    id: Date.now(),
-    name,
-    gender,
-    description
-  })
+    // characters.push({
+    //   id: Date.now(),
+    //   name,
+    //   gender,
+    //   description
+    // })
 
-  res.status(201).end()
+    await connection("Revisao_character")
+      .insert({name, gender, description})
+
+    res.status(201).end()
+  }catch(error:any){
+    res.status(500).send("Unexpected server error")
+  }
 }
--- revisao-mod5/src/endpoints/getAllCharacters.ts
+++ revisao-mod5/src/endpoints/getAllCharacters.ts
@@ -1,7 +1,18 @@
 import { Request,Response } from "express";
-import { characters } from "../dataBase/data";
+import { connection } from "../dataBase/connection";
+import * as types from "../types"
 
 
-export default function( req:Request, res:Response ):void {
-  res.send(characters)
+export default async function getAllCharacters( req:Request, res:Response ): Promise <void> {
+  // const characters: types.Character[] = await connection.raw(`
+  //   SELECT * FROM Revisao_character
+  // `) // Metodo RAW
+  try{
+    const characters: types.Character[] = await connection("Revisao_character") // queryBuild
+
+    res.send(characters)
+  }catch(error:any){
+    res.status(500).send("Unexpected server error")
+  }
+
 }
